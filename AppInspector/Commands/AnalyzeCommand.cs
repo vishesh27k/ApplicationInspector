394a91d007589b0d199c1c7b028f7b28905daacf
Vishesh Kumar <vishesh.kumar@privado.ai>
2024-04-03 23:50:09 +0530

    f

--- AppInspector/Commands/AnalyzeCommand.cs
+++ AppInspector/Commands/AnalyzeCommand.cs
@@ -1,5 +1,4 @@
 ﻿// Copyright (C) Microsoft. All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 using System;
 using System.Collections.Concurrent;
